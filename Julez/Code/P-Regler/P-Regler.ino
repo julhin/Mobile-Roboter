f0049e9c2e57b1dc3859d98d707701f0622585f1
Julian Hinrichs <julian.hinrichs44@gmail.com>
2017-06-19 00:57:53 +0200

    just files added

--- Julez/Code/P-Regler/P-Regler.ino
+++ Julez/Code/P-Regler/P-Regler.ino
@@ -1,26 +1,29 @@
 #include <Asuro.h>
 /* Values need adjustment*/
 #define BASE 100
-#define FACTOR 3
-#define SWITCH_BLACK_WHITE 23
+//#define FACTOR 3
+//#define SWITCH_BLACK_WHITE 23
 Asuro asuro = Asuro();
 /*global values */
-unsigned int delta;
-unsigned int soll_Wert;
-unsigned int ist_Wert;
+//unsigned int delta;
+//unsigned int soll_Wert;
+//unsigned int ist_Wert;
 
-unsigned int odo_data [2];
-unsigned int line_data [2];
-unsigned int speed_data[2];
+//unsigned int odo_data [2];
+int line_data [2];
+//unsigned int speed_data[2];
 
 /*alle benötigten Informationen sind vorhanden */
 void p_Regler(){
 asuro.readLinesensor(line_data);
 
+int diff = line_data[0] - line_data[1];
+int leftSpeed = BASE + diff ;
+int rightSpeed = BASE - diff;
 
- 
+asuro.setMotorSpeed(leftSpeed, rightSpeed);
 }
-
+/*
 void measureDelta(){
   for (int i = 0; i < 500; i++){
   asuro.setMotorDirection(FWD,FWD);
@@ -28,20 +31,23 @@ void measureDelta(){
   }
    asuro.readLinsensor(line_data);
    delta = abs(line_data[0] - line_data[0]);
+    asuro.setMotorSpeed (0,0);
 }
-
-
+*/
 
 void setup() {
 asuro.Init();
  delay(200);
  Serial.begin(2400);
  delay(200);
- measureDelta()
+ asuro.setMotorDirection(FWD,FWD);
+  asuro.setMotorSpeed (BASE,BASE);
+  delay(2000);
 }
 
 void loop() {
-
+  asuro.setMotorDirection(FWD,FWD);
+  asuro.setMotorSpeed (BASE,BASE);
 while(1){
   p_Regler();
 }
