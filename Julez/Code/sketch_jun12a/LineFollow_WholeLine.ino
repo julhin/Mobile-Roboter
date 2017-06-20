ad36e128edd6b5954dda4da8af6955349ffec27e
Julian Hinrichs <julez@scc-wkit-clx-236-63.scc.kit.edu>
2017-06-20 09:12:45 +0200

    linux has to commit

--- Julez/Code/sketch_jun12a/LineFollow_WholeLine.ino
+++ Julez/Code/sketch_jun12a/LineFollow_WholeLine.ino
@@ -1,47 +1,57 @@
 #include <Asuro.h>
 /* Values need adjustment*/
-#define FAST_SPEED 20
-#define SLOW_SPEED 10
+#define STRAIGHT_DRIVING 20
+#define SPEED 100
+#define BLACK 800
+#define WHITE 200
+#define SPEED_ALTERING_VALUE 5
 
 Asuro asuro = Asuro();
-unsigned int current_speed [2];
 int line_sensors[2];
+unsigned int current_speed [2];
 
- void controlSpeed(){
-/* if the sensors are more than 20 away  from each other, one sees black and the other sees black */
-int temp = line_sensors[0] - line_sensors[1] ;
- temp = abs(temp);
-if (temp > 20){
-  if (line_sensors[0] < line_sensors[1]){
-    /* left speed must be faster */
-   current_speed[0] = FAST_SPEED;
-   current_speed[1] = SLOW_SPEED;
-    
-  }else if (line_sensors[0] > line_sensors[1]){
-    /* right  has to be faster */
-    current_speed[0] = SLOW_SPEED;
-    current_speed[1] = FAST_SPEED;
-  }
-} else {
-   current_speed[0] = FAST_SPEED; 
-   current_speed[1] = FAST_SPEED;
+ void drive(){
+   int diff = line_sensors[0] - line_sensors[1];
+   diff = abs(diff);
+
+   if (diff < STRAIGHT_DRIVING  ){
+     current_speed[0]= SPEED;
+     current_speed[1]= SPEED;
+   } else if (line_sensors[0] < line_sensors[1] && line_sensors[1] > BLACK){
+     // the left sensor is on white, while right sensor is on black
+     // right need to be braked
+     current_speed[0]= SPEED + SPEED_ALTERING_VALUE;
+     current_speed[1]= SPEED - SPEED_ALTERING_VALUE;
+   } else if (line_sensors[0] > line_sensors[1] && line_sensors[0] > BLACK){
+   current_speed[0]= SPEED - SPEED_ALTERING_VALUE;
+   current_speed[1]= SPEED + SPEED_ALTERING_VALUE;
+ } else if (line_sensors[0] < WHITE && line_sensors[1] < WHITE){
+ // line lost
+      findLineAgain();
+ }
+ }
+
+void findLineAgain(){
+  asuro.setMotorDirection(FWD,RWD);
+  asuro.setMotorSpeed(SPEED,SPEED);
 }
-}
+
 void setup() {
   asuro.Init();
   delay(200);
   Serial.begin(2400);
   delay(200);
  asuro.setMotorDirection(FWD,FWD);
- current_speed[1] = FAST_SPEED;
- current_speed[0] = FAST_SPEED;
- 
+ current_speed[0] = SPEED;
+ current_speed[1] = SPEED;
+
 }
 
 void loop() {
+  asuro.setMotorSpeed(SPEED,SPEED);
+  while(1){
   asuro.readLinesensor(line_sensors);
-  asuro.setMotorSpeed(FAST_SPEED,FAST_SPEED);
-  while(1);
-  controlSpeed();
-  asuro.setMotorSpeed (current_speed [0], current_speed[1]); 
+  drive();
+  asuro.setMotorSpeed (current_speed[0], current_speed[1]);
+  }
 }
