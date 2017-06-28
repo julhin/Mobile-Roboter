57c601770f9fa4245a4c62444e4233095a358e2f
Julian Hinrichs <julian.hinrichs44@gmail.com>
2017-06-28 17:18:52 +0200

    swome files added

---
+++ Julez/Code/MessProgramme/sketch_jun28b/lineSensor.ino
@@ -0,0 +1,25 @@
+#include <Asuro.h>
+
+Asuro asuro = Asuro();
+
+unsigned int line_data[2];
+
+void setup(){
+  asuro.Init();
+  delay(200);
+  Serial.begin(2400);
+  delay(200);
+    asuro.setFrontLED(ON);
+  delay(200);
+}
+
+void loop(){
+
+  while(1){
+    asuro.readLinesensor(line_data);
+    Serial.print(line_data[0]);
+    Serial.print(",");
+    Serial.print(line_data[1]);
+    Serial.print("\n");
+  }
+}
