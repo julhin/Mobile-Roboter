a498c7b5826d653ea068e7e0ad0a538ada173027
Benkler <benklerniko@t-online.de>
2017-06-29 13:41:40 +0200

    FINAL VERSION!

--- MainProgramm/MainProgramm.ino
+++ MainProgramm/MainProgramm.ino
@@ -3,16 +3,19 @@
 #define BARCODETHRESHOLD 450
 #define MINLINECONTRAST 80
 #define BARCODECONTRAST 30
-#define ODOTHRESHOLD 80
+#define ODOTHRESHOLD 120
 #define BRIGHT 0
 #define DARK 1
 #define SEGMENTS 40
 #define WHEELPERIMETER 12
-#define MAXSPEED 120
-#define MAXSPEEDTURN 100
-#define PGAINFORWARD 2
+#define MAXSPEED 170
+#define MAXSPEEDTURN 150
 
-#define BREAKDELTA 60
+#define MAXSPEEDBARCODE 200
+#define PGAINFORWARD 2
+#define BARCODEDISTANCE 4
+
+#define BREAKDELTA 120
 
 Asuro asuro = Asuro();
 enum STATES{followLine, searchLine,scanBarcode, blinkNTimes,stop, findLine, error};
@@ -38,106 +41,92 @@ struct wheel right = {0, 0, DARK};
 /*
   * Method to calculate ticks, this is needed to drive straight  forward
   */
- void calculateTicks(struct wheel *rad, int identifier){  // identifier 0 = left wheel, 1 = tight wheel
-    asuro.readOdometry(odo_data);  // Odometrie auslesen fuer ein Rad
-    switch(rad->flag) {
-      case DARK:
-      if(odo_data[identifier] > rad->lastMax_val)  rad->lastMax_val = odo_data[identifier]; // neuer Max wenn vorher Dunkel
-      break;
-      case BRIGHT:
-      if(odo_data[identifier] < rad->lastMax_val)  rad-> lastMax_val = odo_data[identifier]; // neuer Max wenn vorher Hell
-      break;
-      default:  //Error Handling?
-      break;
+void calculateTicks(struct wheel *rad, int identifier){  // identifier 0 = left wheel, 1 = tight wheel
+  asuro.readOdometry(odo_data);  // Odometrie auslesen fuer ein Rad
+  switch(rad->flag) {
+   case DARK:
+    if(odo_data[identifier] > rad->lastMax_val)  rad->lastMax_val = odo_data[identifier]; // neuer Max wenn vorher Dunkel
+    break;
+   case BRIGHT:
+    if(odo_data[identifier] < rad->lastMax_val)  rad-> lastMax_val = odo_data[identifier]; // neuer Max wenn vorher Hell
+    break;
+   default:  //Error Handling?
+   break;
     }
-   unsigned int diff = rad->lastMax_val - odo_data[identifier]; // calculate difference
-   diff = abs(diff); 
-   if (diff > ODOTHRESHOLD){  //if so -> plus one tick!
-   rad->encoder_ticks += 1;
-   rad->flag = (rad->flag + 1)%2; //wird von 0 auf 1 oder von 1 auf 0 gesetzt
+  unsigned int diff = rad->lastMax_val - odo_data[identifier]; // calculate difference
+  diff = abs(diff); 
+  if (diff > ODOTHRESHOLD){  //if so -> plus one tick!
+  rad->encoder_ticks += 1;
+  rad->flag = (rad->flag + 1)%2; //wird von 0 auf 1 oder von 1 auf 0 gesetzt
    }
  }
-void driveStraight(){
-    calculateTicks(&left,0);
-    calculateTicks(&right,1);
-    asuro.setMotorDirection(FWD,FWD);
-    int diff = left.encoder_ticks - right.encoder_ticks;
-    //Speed regeln
-    if(diff < 0 ){
-    diff = abs(diff);
-    asuro.setMotorSpeed(MAXSPEED + PGAINFORWARD*diff,MAXSPEED - PGAINFORWARD*diff);
-    }else{
-    diff = abs(diff);
-    asuro.setMotorSpeed(MAXSPEED - PGAINFORWARD*diff,MAXSPEED + PGAINFORWARD*diff);
-    }    
-    
+void driveStraight(int givenSpeed){
+  calculateTicks(&left,0);
+  calculateTicks(&right,1);
+  asuro.setMotorDirection(FWD,FWD);
+  int diff = left.encoder_ticks - right.encoder_ticks;
+  //Speed regeln
+  if(diff < 0 ){
+  diff = abs(diff);
+  asuro.setMotorSpeed(givenSpeed + PGAINFORWARD*diff,givenSpeed - PGAINFORWARD*diff);
+  }else{
+  diff = abs(diff);
+  asuro.setMotorSpeed(givenSpeed - PGAINFORWARD*diff,givenSpeed + PGAINFORWARD*diff);
+  }       
 }
-void driveStraightCentimeter(unsigned int cm){
-   left.encoder_ticks=0;  //reset
-   right.encoder_ticks=0;
-   asuro.setMotorDirection(FWD,FWD);
-   asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
 
-   unsigned int encoderTicksDesiredValue = (SEGMENTS / WHEELPERIMETER) * cm;
-   unsigned int encoderTicksCurrentValue = 0;
-  
-   while(encoderTicksCurrentValue < encoderTicksDesiredValue){
-    driveStraight();
-    encoderTicksCurrentValue = ((left.encoder_ticks) + (right.encoder_ticks)) / 2;
-   }
-  
- }
+void driveStraightCentimeter(unsigned int cm, int givenSpeed){
+  left.encoder_ticks=0;  //reset
+  right.encoder_ticks=0;
  
-
-
+  unsigned int encoderTicksDesiredValue = (SEGMENTS / WHEELPERIMETER) * cm;
+  unsigned int encoderTicksCurrentValue = 0;
+  while(encoderTicksCurrentValue < encoderTicksDesiredValue){
+   driveStraight(givenSpeed);
+   encoderTicksCurrentValue = ((left.encoder_ticks) + (right.encoder_ticks)) / 2;
+  }
+}
+ 
 /*
  * Turn left/right (90 degree) forwards/backwards and scan for Line
  */
-boolean turnLeftAndScanForLine(int leftwheel,int rightwheel){
+boolean turnLeftAndScanForLine(int leftWheelDir,int rightWheelDir){
   left.encoder_ticks=0;  //reset
   right.encoder_ticks=0;
-  asuro.setMotorDirection(leftwheel, rightwheel);
+  asuro.setMotorDirection(leftWheelDir, rightWheelDir);
   asuro.setMotorSpeed(MAXSPEEDTURN,MAXSPEEDTURN);
-  unsigned int encoderTicksDesiredValue = 25;  //has to be improved
+  unsigned int encoderTicksDesiredValue = 30;  //has to be improved
   unsigned int encoderTicksCurrentValue = 0; 
   while(encoderTicksCurrentValue < encoderTicksDesiredValue){
-   
    if(isOnLine()) {   
-   
    asuro.setMotorDirection(BREAK,BREAK);
    return true;
    }
    calculateTicks(&right,1);
-   encoderTicksCurrentValue = right.encoder_ticks;
+   calculateTicks(&left,0);
+   encoderTicksCurrentValue = (right.encoder_ticks+left.encoder_ticks)/2;
   }
-  return false;
+return false;
 }
 
-boolean turnRightAndScanForLine(int leftwheel,int rightwheel){
+boolean turnRightAndScanForLine(int leftWheelDir,int rightWheelDir){
   left.encoder_ticks=0;  //reset
   right.encoder_ticks=0;
-  asuro.setMotorDirection(leftwheel, rightwheel);
+  asuro.setMotorDirection(leftWheelDir, rightWheelDir);
   asuro.setMotorSpeed(MAXSPEEDTURN,MAXSPEEDTURN);
-  unsigned int encoderTicksDesiredValue = 25;  //has to be improved
+  unsigned int encoderTicksDesiredValue = 30;  //has to be improved
   unsigned int encoderTicksCurrentValue = 0; 
   while(encoderTicksCurrentValue < encoderTicksDesiredValue){
-    
    if(isOnLine()) {   
-     
    asuro.setMotorDirection(BREAK,BREAK);
    return true;
    }
    calculateTicks(&left,0);
-   encoderTicksCurrentValue = left.encoder_ticks;
+  calculateTicks(&right,1);
+  encoderTicksCurrentValue = (right.encoder_ticks+left.encoder_ticks)/2;
   }
-  return false;
+return false;
 }
-
-
-
-
-
- 
 
 /*
  * Start state: Drive straight, search Line 
@@ -146,36 +135,33 @@ boolean turnRightAndScanForLine(int leftwheel,int rightwheel){
 void task_findLine(){
   left.encoder_ticks=0;  //reset
   right.encoder_ticks=0;
- while(isOnLine() == 0){
-  driveStraight();
+  while(isOnLine() == 0){
+  driveStraight(MAXSPEED);
  }
  current_state = followLine;
 }
 
+/**
+ * Follow line with line sensors
+ */
 void task_followLine(){
-  asuro.readLinesensor(lineData);  //read sensor data
-  asuro.setMotorDirection(FWD,FWD);
-   asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
-  int diff = lineData[0] - lineData[1];
-  diff = abs(diff);  //workaround!  No Calculation within abs() allowed
+ asuro.readLinesensor(lineData);  //read sensor data
+ asuro.setMotorDirection(FWD,FWD);
+ asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
+ int diff = lineData[0] - lineData[1];
+ diff = abs(diff);  //workaround!  No Calculation within abs() allowed
    /**
    * if delta of both sensor is big enough -> asuro detected the edge of the line
    */
-  if(isOnLine()){    
-   // if(diff > MINLINECONTRAST){               
-   oldLineData[0] = lineData[0];            //copy array to update old values
-   oldLineData[1] = lineData[1];
-   asuro.setMotorDirection(FWD,FWD);
-   asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
-   }else{
-    if(oldLineData[0] > oldLineData[1]){      // check previous calculation  
-      asuro.setMotorSpeed(MAXSPEED,MAXSPEED-BREAKDELTA);
-      current_state = searchLine;
-      }else{
-      asuro.setMotorSpeed(MAXSPEED - BREAKDELTA,MAXSPEED);
-      current_state = searchLine;
-      }
-   }
+ if(isOnLine()){                  
+ oldLineData[0] = lineData[0];            //copy array to update old values
+ oldLineData[1] = lineData[1];
+ asuro.setMotorDirection(FWD,FWD);
+ asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
+ }else{
+  asuro.setMotorDirection(BREAK,BREAK);
+  current_state = searchLine;
+ }
 }
 
 void task_searchLine(){
@@ -184,26 +170,21 @@ void task_searchLine(){
     return;
   }
   asuro.setMotorDirection(BREAK,BREAK);
- 
   //check previous calculation -> decision if left or right turn
   if(oldLineData[0] < oldLineData[1]){ 
-    if(turnLeftAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(FWD,RWD) || turnRightAndScanForLine(FWD,RWD) || turnRightAndScanForLine(RWD,FWD) ){  //scan starting left
-    asuro.setMotorDirection(FWD,FWD);
-    asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
-    current_state = followLine;
-    return;
-    }
-   current_state = scanBarcode;
+   if(turnLeftAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(FWD,RWD) || turnRightAndScanForLine(FWD,RWD) || turnRightAndScanForLine(RWD,FWD) ){  //scan starting left
+   current_state = followLine;
    return;
+   }
+  current_state = scanBarcode;
+  return;
   }else{
-    if(turnRightAndScanForLine(FWD,RWD) || turnRightAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(FWD,RWD) ){  //scan starting right
-    asuro.setMotorDirection(FWD,FWD);
-    asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
-    current_state = followLine;
-    return;
-    }
-   current_state = scanBarcode;
+   if(turnRightAndScanForLine(FWD,RWD) || turnRightAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(RWD,FWD) || turnLeftAndScanForLine(FWD,RWD) ){  //scan starting right
+   current_state = followLine;
    return;
+   }
+  current_state = scanBarcode;
+  return;
   }
 }
 
@@ -214,7 +195,7 @@ boolean isOnLine(){
    asuro.readLinesensor(lineData);
    int diff = lineData[0] - lineData[1];
    diff = abs(diff);
-   if((lineData[0] + lineData[1]) < WHITETHRESHOLD && diff > MINLINECONTRAST) {   //TODO: Werte anpassen?!
+   if((lineData[0] + lineData[1]) < WHITETHRESHOLD && diff > MINLINECONTRAST) {   
    //if(diff > MINLINECONTRAST) {  
    return true;
    }
@@ -226,13 +207,9 @@ boolean isOnBarcode(){
    asuro.readLinesensor(lineData);
    int diff = lineData[0] - lineData[1];
    diff = abs(diff);
-   if((lineData[0] + lineData[1]) < BARCODETHRESHOLD && diff < BARCODECONTRAST) {   //TODO: Werte anpassen
-  // Serial.println("Ist auf Barcode");
-  // delay(1000);
+   if((lineData[0] + lineData[1]) < BARCODETHRESHOLD && diff < BARCODECONTRAST) {   
    return true;
-   
    }
-   //Serial.println("Barcode false");
    return false;
 }
 
@@ -244,74 +221,59 @@ boolean isOnBarcode(){
 boolean driveStraightAndScanForBarcode(unsigned int cm){
    left.encoder_ticks=0;  //reset
    right.encoder_ticks=0;
-   asuro.setMotorDirection(FWD,FWD);
-   asuro.setMotorSpeed(MAXSPEED,MAXSPEED);
-
    unsigned int encoderTicksDesiredValue = (SEGMENTS / WHEELPERIMETER) * cm;
    unsigned int encoderTicksCurrentValue = 0;
-  
    while(encoderTicksCurrentValue < encoderTicksDesiredValue){
     if(isOnBarcode()) { 
-    //Serial.print("Innerhalb driveStraight: ");
-    //Serial.println("isOnBarcode");
     return true;
     }
-    driveStraight();
+    driveStraight(MAXSPEEDBARCODE);
     encoderTicksCurrentValue = ((left.encoder_ticks) + (right.encoder_ticks)) / 2;
    }
-   //Serial.println(false);
+   
    return false;
  }
-
 
 
 void crossBarcode(){
    left.encoder_ticks=0;  //reset
    right.encoder_ticks=0;
-   driveStraightCentimeter(4);
+   driveStraightCentimeter(2, MAXSPEEDBARCODE);  //ANPASSEN!!!
    
  }
 
 void task_scanBarcode(){
- 
   enum BARSTATES{onbar, offbar, goToBar};
   BARSTATES barstate = goToBar; //init
   barCount = 0; //reset 
   while(true){
-    switch(barstate){
-     case goToBar:
-       if(driveStraightAndScanForBarcode(9)){
-       barstate = onbar;
-       }else{
-       current_state = error;
-       return;
-       }
-      break;
-     case onbar:
-      // Serial.println("onbar");
-       crossBarcode();
-       barCount++;
-       barstate = offbar;
-      break;
-     case offbar:
-       // Serial.println("driveStraightAndScanForBarcode: ");
-        //Serial.println(driveStraightAndScanForBarcode(5));
-       if(driveStraightAndScanForBarcode(5)){  //scan for next barcode
-       //Serial.println("offbar");
-       barstate = onbar;
-       }else{
-       //Serial.println("blinkNTimes");
-       current_state = blinkNTimes;
-       return; 
-       }
-      break;
+  switch(barstate){
+   case goToBar:
+    if(driveStraightAndScanForBarcode(9)){
+    barstate = onbar;
+    }else{
+    current_state = error;
+    return;
+    }
+    break;
+   case onbar:
+    crossBarcode();
+    barCount++;
+    barstate = offbar;
+    break;
+   case offbar:
+    if(driveStraightAndScanForBarcode(BARCODEDISTANCE)){  //scan for next barcode
+    barstate = onbar;
+    }else{
+    current_state = blinkNTimes;
+    return; 
+    }
+    break;
     }
    }
-   
 }
 
 void task_blinkNTimes(){
-  //Serial.println(barCount);
   asuro.setMotorDirection(BREAK,BREAK);
   if(barCount == 1){
     asuro.setBackLED(ON,ON);
@@ -334,17 +296,19 @@ void task_blinkNTimes(){
 }
 
 void task_stop(){
-    asuro.setMotorDirection(BREAK,BREAK);
-      asuro.setStatusLED(GREEN);
+  asuro.setMotorDirection(BREAK,BREAK);
+  asuro.setStatusLED(GREEN);
 }
+
 void task_error(){
   asuro.setMotorDirection(BREAK,BREAK);
-      asuro.setStatusLED(RED);
+  asuro.setStatusLED(RED);
 }
+
 void task_default(){
   asuro.setMotorDirection(BREAK,BREAK);
-      asuro.setStatusLED(RED);
-      asuro.setBackLED(ON,ON);
+  asuro.setStatusLED(RED);
+  asuro.setBackLED(ON,ON);
 }
 
 
