6e529a91520c597b647e7c35380a73fc14b9beaa
Julian Hinrichs <julian.hinrichs44@gmail.com>
2017-07-04 23:39:47 +0200

    working code yay
    some coding done

--- Julez/Code/LineFollow_WholeLine/LineFollow_WholeLine.ino
+++ Julez/Code/LineFollow_WholeLine/LineFollow_WholeLine.ino
@@ -1,57 +1,84 @@
 #include <Asuro.h>
 /* Values need adjustment*/
-#define STRAIGHT_DRIVING 20
-#define SPEED 100
-#define BLACK 800
-#define WHITE 200
-#define SPEED_ALTERING_VALUE 5
+// P regler funktioniert bei mir besser als der Bauern ansatz
+//
+#define KP 0.2
+#define BASE 100
+#define TICKS_ANGLE 30
+#define ODOTHRESHOLD 80
 
 Asuro asuro = Asuro();
-int line_sensors[2];
-unsigned int current_speed [2];
 
- void drive(){
-   int diff = line_sensors[0] - line_sensors[1];
-   diff = abs(diff);
+unsigned int l_data[2];
+unsigned int speed[2];
 
-   if (diff < STRAIGHT_DRIVING  ){
-     current_speed[0]= SPEED;
-     current_speed[1]= SPEED;
-     
-   } else if (line_sensors[0] < line_sensors[1] ){
-     current_speed[0]= SPEED + SPEED_ALTERING_VALUE;
-     current_speed[1]= SPEED - SPEED_ALTERING_VALUE;
-     
-   } else if (line_sensors[0] > line_sensors[1] ){
-   current_speed[0]= SPEED - SPEED_ALTERING_VALUE;
-   current_speed[1]= SPEED + SPEED_ALTERING_VALUE;
-   
- } else if (line_sensors[0] < WHITE && line_sensors[1] < WHITE){
-  findLineAgain();
- }
- }
+unsigned int odo_data[2];
+unsigned int old_odo_data[2];
+unsigned int ticks[2];
 
-void findLineAgain(){
-  asuro.setMotorDirection(FWD,RWD);
-  asuro.setMotorSpeed(SPEED,SPEED);
+void findTick(int id){
+  int diff = odo_data[id] - old_odo_data[id];
+  diff = abs(diff);
+  if (diff > ODOTHRESHOLD ){
+    // tick found
+    ticks[id]++;
+  }
+  old_odo_data[id] = odo_data[id];
+}
+void turnLeft(){
+  asuro.readOdometry(old_odo_data);
+  // asuro on the move
+  asuro.setMotorDirection(FWD,FWD);
+  asuro.setMotorSpeed(0,BASE);
+  while(1){
+      asuro.readOdometry(odo_data);
+      findTick(1);
+      Serial.print(ticks[1]);
+      if(ticks[1] > TICKS_ANGLE){
+        // enough ticks, reset ticks, stop the asuro moving
+        Serial.print("Hello");
+        asuro.setMotorSpeed(0,0);
+        ticks[0] = 0;
+        ticks[1] = 0;
+        break;
+      }
+  }
+  asuro.setStatusLED(YELLOW);
+  delay (500);
+
 }
 
-void setup() {
+void p_regler(){
+  asuro.readLinesensor(l_data);
+  int diff = l_data[0] - l_data[1];
+  speed[0] = BASE + diff * KP;
+  speed[1] = BASE - diff * KP;
+}
+
+void setup(){
   asuro.Init();
   delay(200);
   Serial.begin(2400);
   delay(200);
- asuro.setMotorDirection(FWD,FWD);
- current_speed[0] = SPEED;
- current_speed[1] = SPEED;
-
+    asuro.setFrontLED(ON);
+  delay(200);
 }
 
-void loop() {
-  asuro.setMotorSpeed(SPEED,SPEED);
-  while(1){
-  asuro.readLinesensor(line_sensors);
-  drive();
-  asuro.setMotorSpeed (current_speed[0], current_speed[1]);
+void loop(){
+  ticks[0] = 0;
+  ticks[1] = 0;
+
+  speed[0] = BASE;
+  speed[1] = BASE;
+
+  asuro.setMotorDirection(FWD,FWD);
+  asuro.setMotorSpeed(BASE,BASE);
+    turnLeft();
+ while(1){
+   asuro.setMotorDirection(RWD,FWD);
+   asuro.setMotorSpeed(BASE,BASE);
+  //  p_regler();
+  //  asuro.setMotorSpeed(speed[0], speed[1]);
   }
+
 }
